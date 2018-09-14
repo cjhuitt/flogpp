257d7511a0face46706633544e4ef0aa76b83837
Caleb Huitt <chuitt@agleader.com>
2018-09-14 13:37:02 -0500

    Add some tests for potential pitfalls

--- test/snippet_analyzer_test.rb
+++ test/snippet_analyzer_test.rb
@@ -7,4 +7,28 @@ class SnippetAnalyzerTest < Minitest::Test
     function_analyzer = SnippetAnalyzer.new( code )
     assert_equal function_analyzer.analyze(), 0
   end
+
+  def test_finds_zero_for_return_keyword
+    code = "return 37;"
+    function_analyzer = SnippetAnalyzer.new( code )
+    assert_equal function_analyzer.analyze(), 0
+  end
+
+  def test_finds_zero_for_default_parameters
+    code = "void foo( int a = 0, float b = 0.05f ) { }"
+    function_analyzer = SnippetAnalyzer.new( code )
+    assert_equal function_analyzer.analyze(), 0
+  end
+
+  def test_finds_zero_for_c_comment
+    code = "/* a = foo(); */"
+    function_analyzer = SnippetAnalyzer.new( code )
+    assert_equal function_analyzer.analyze(), 0
+  end
+
+  def test_finds_zero_for_cpp_comment
+    code = "// a = foo();"
+    function_analyzer = SnippetAnalyzer.new( code )
+    assert_equal function_analyzer.analyze(), 0
+  end
 end
