51f6b016e6e8de351ca4ca9dc5dbf3d27881a9f2
Caleb Huitt <development-caleb@huitt.org>
2018-09-15 15:24:56 -0500

    Distinguish assignments

--- lib/snippet_analyzer.rb
+++ lib/snippet_analyzer.rb
@@ -1,27 +1,31 @@
 class SnippetAnalyzer
-  attr_reader :score
+  attr_reader :assignments
 
   def initialize( code )
-    @score = 0
+    @assignments = 0
     code.gsub! /\/\/.*$/, ""            # C++ Comments
     code.gsub! /\/\*.*\*\//, ""         # C Comments
     code.gsub! /const\s+[\w:]+\s*[\w:]+\s*=\s*[\d.]+\s*;/, ""  # const variable assignment
-    @score = 1 if code.include? "catch"
+    @assignments = 1 if code.include? "catch"
     code.gsub! /[\w:]+\s*\(.*\)\s*{/, ""  # Function declarations
-    @score = 1 if code.include? "="
-    @score = 1 if code.include? "++"
-    @score = 1 if code.include? "--"
-    @score = 1 if code.include? "<"
-    @score = 1 if code.include? ">"
-    @score = 1 if code.include? "else"
-    @score = 1 if code.include? "case"
-    @score = 1 if code.include? "default"
-    @score = 1 if code.include? "try"
-    @score = 1 if /^\s*\w+\s*$/.match? code      # unary conditions
-    @score = 1 if /(^|\s)(((::)?\w+)+)\s*\(\s*(([\w]+(\.|->)?)\s*,?\s*)*\s*\)/.match? code      # scoped function calls
-    @score = 1 if /(^|\s)((\w+)(\.|->)?)+\s*\(\s*(([\w]+(\.|->)?)\s*,?\s*)*\s*\)/.match? code   # pointer/instance function calls
-    @score = 2 if /\snew\s/.match? code
-    @score = 2 if /\sdelete\s/.match? code
-    @score = 3 if code.include? "goto"
+    @assignments = 1 if code.include? "="
+    @assignments = 1 if code.include? "++"
+    @assignments = 1 if code.include? "--"
+    @assignments = 1 if code.include? "<"
+    @assignments = 1 if code.include? ">"
+    @assignments = 1 if code.include? "else"
+    @assignments = 1 if code.include? "case"
+    @assignments = 1 if code.include? "default"
+    @assignments = 1 if code.include? "try"
+    @assignments = 1 if /^\s*\w+\s*$/.match? code      # unary conditions
+    @assignments = 1 if /(^|\s)(((::)?\w+)+)\s*\(\s*(([\w]+(\.|->)?)\s*,?\s*)*\s*\)/.match? code      # scoped function calls
+    @assignments = 1 if /(^|\s)((\w+)(\.|->)?)+\s*\(\s*(([\w]+(\.|->)?)\s*,?\s*)*\s*\)/.match? code   # pointer/instance function calls
+    @assignments = 2 if /\snew\s/.match? code
+    @assignments = 2 if /\sdelete\s/.match? code
+    @assignments = 3 if code.include? "goto"
+  end
+
+  def score
+    @assignments
   end
 end
