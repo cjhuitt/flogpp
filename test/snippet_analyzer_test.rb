4f33d6b18438636a6c0c072fe3d7e51892a2bb25
Caleb Huitt <development-caleb@huitt.org>
2018-09-14 21:35:12 -0500

    Rename test class for focus on simple code blocks

--- test/snippet_analyzer_test.rb
+++ test/snippet_analyzer_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative '../lib/snippet_analyzer'
 
-class SnippetAnalyzerTest < Minitest::Test
+class SimpleSnippetAnalyzerTest < Minitest::Test
   def test_finds_zero_for_empty_function
     code = "void foo() { }"
     function_analyzer = SnippetAnalyzer.new( code )
