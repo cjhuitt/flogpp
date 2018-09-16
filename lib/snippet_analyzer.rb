786cda27a2dbc9edb988d54f2f9e5f8a0bdb2df4
Caleb Huitt <development-caleb@huitt.org>
2018-09-16 11:52:06 -0500

    Simplify function detection by removing pointer redirection check.

--- lib/snippet_analyzer.rb
+++ lib/snippet_analyzer.rb
@@ -82,8 +82,8 @@ class SnippetAnalyzer
     end
 
     def check_branches_in code
-      @branches = 1 if /(^|\s)(\w+)\s*\(\s*(([\w]+(\.|->)?)\s*,?\s*)*\s*\)/.match? code      # scoped function calls
-      @branches = 1 if /(^|\s)((\w+)(\.|->)?)+\s*\(\s*(([\w]+(\.|->)?)\s*,?\s*)*\s*\)/.match? code   # pointer/instance function calls
+      @branches = 1 if /(^|\s)(\w+)\s*\(\s*(([\w]+\.?)\s*,?\s*)*\s*\)/.match? code      # scoped function calls
+      @branches = 1 if /(^|\s)((\w+)\.?)+\s*\(\s*(([\w]+\.?)\s*,?\s*)*\s*\)/.match? code   # pointer/instance function calls
       @branches = 2 if /\snew\s/.match? code
       @branches = 2 if /\sdelete\s/.match? code
       @branches = 3 if code.include? "goto"
